a02f0d48a2c9e4b9cb88c9e2e11ea574336816c8
Jayceed Deyro <66674372+jsdeyro@users.noreply.github.com>
2023-12-27 03:05:48 +0800

    Initial Commit

---
+++ app/src/main/java/ph/com/globe/ihdextentionslibrary/common/CommonExtensions.kt
@@ -0,0 +1,15 @@
+package ph.com.globe.ihdextentionslibrary.common
+
+import android.view.View
+
+fun View.gone() {
+    visibility = View.GONE
+}
+
+fun View.hide() {
+    visibility = View.INVISIBLE
+}
+
+fun View.show() {
+    visibility = View.VISIBLE
+}
